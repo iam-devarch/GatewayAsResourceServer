375c38a8a38ae0da552d144cdf82e2bc26ffaf46
khan <somemail>
2023-04-09 21:28:04 +0800

    Initial commit

---
+++ src/main/java/com/example/gatewayasresourceserver/GatewayAsResourceServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.gatewayasresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayAsResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayAsResourceServerApplication.class, args);
+	}
+
+}
+  
---
+++ src/test/java/com/example/gatewayasresourceserver/GatewayAsResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewayasresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayAsResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
